fd3b377f8fe8ce40fd2478193e4a46f3d80a8767
phuc-create <phuc82908@gmail.com>
2024-05-18 16:42:50 +0700

    init cws (chat with sam)

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-indigo-500">
+      hello chat with sam project
+    </main>
+  )
+}
