e289cc9824579c2db781721eb2892280f60898bd
phuc-create <phuc82908@gmail.com>
2024-05-22 16:58:56 +0700

    try to format a file

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -17,15 +17,15 @@ interface ServerSidebarProps {
 }
 
 const iconMap: { [x in CHANNEL_TYPE]: React.ReactNode } = {
-  TEXT: <Hash className='mr-2 h-4 w-4' />,
-  AUDIO: <Mic className='mr-2 h-4 w-4' />,
-  VIDEO: <Video className='mr-2 h-4 w-4' />
+  TEXT: <Hash className="mr-2 h-4 w-4" />,
+  AUDIO: <Mic className="mr-2 h-4 w-4" />,
+  VIDEO: <Video className="mr-2 h-4 w-4" />
 }
 
 const roleToIcon: { [u in MEMBER_ROLE]: React.ReactNode | null } = {
   GUEST: null,
-  ADMIN: <ShieldAlert className='h-4 w-4 mr-2 text-rose-500' />,
-  MODERATOR: <ShieldCheck className='h-4 w-4 ml-2 text-emerald-500' />
+  ADMIN: <ShieldAlert className="mr-2 h-4 w-4 text-rose-500" />,
+  MODERATOR: <ShieldCheck className="ml-2 h-4 w-4 text-emerald-500" />
 }
 
 const ServerSidebar: React.FC<ServerSidebarProps> = async ({ serverID }) => {
@@ -44,7 +44,7 @@ const ServerSidebar: React.FC<ServerSidebarProps> = async ({ serverID }) => {
     include: {
       channels: {
         orderBy: {
-          createdAt: "asc"
+          createdAt: 'asc'
         }
       },
       members: {
@@ -52,82 +52,105 @@ const ServerSidebar: React.FC<ServerSidebarProps> = async ({ serverID }) => {
           profile: true
         },
         orderBy: {
-          role: "asc"
+          role: 'asc'
         }
       }
     }
   })
-  if (!server) redirect("/")
+  if (!server) redirect('/')
 
-  const textChannels = server.channels.filter(cn => cn.type === CHANNEL_TYPE.TEXT)
-  const videoChannels = server.channels.filter(cn => cn.type === CHANNEL_TYPE.VIDEO)
-  const audioChannels = server.channels.filter(cn => cn.type === CHANNEL_TYPE.AUDIO)
+  const textChannels = server.channels.filter(
+    cn => cn.type === CHANNEL_TYPE.TEXT
+  )
+  const videoChannels = server.channels.filter(
+    cn => cn.type === CHANNEL_TYPE.VIDEO
+  )
+  const audioChannels = server.channels.filter(
+    cn => cn.type === CHANNEL_TYPE.AUDIO
+  )
 
   const members = server.members.filter(mb => mb.profileID !== profile.id)
   const role = server.members.find(mb => mb.profileID === profile.id)?.role
   return (
-    <div className='flex flex-col text-primary w-full'>
+    <div className="flex w-full flex-col text-primary">
       <ServerHeader server={server} role={role} />
-      <ScrollArea className='flex-1  px-3'>
-        <div className='mt-2'>
-          <ServerSearch data={[{
-            label: "Chat Channels",
-            type: "channel",
-            data: textChannels.map(c => ({
-              id: c.id,
-              name: c.name,
-              icon: iconMap[c.type]
-            }))
-          }, {
-            label: "Audio Channels",
-            type: "channel",
-            data: audioChannels.map(c => ({
-              id: c.id,
-              name: c.name,
-              icon: iconMap[c.type]
-            }))
-          }, {
-            label: "Video Channels",
-            type: "channel",
-            data: videoChannels.map(c => ({
-              id: c.id,
-              name: c.name,
-              icon: iconMap[c.type]
-            }))
-          }, {
-            label: "Members",
-            type: "member",
-            data: members.map(m => ({
-              id: m.id,
-              name: m.profile.name,
-              icon: roleToIcon[m.role]
-            }))
-          }]} />
+      <ScrollArea className="flex-1  px-3">
+        <div className="mt-2">
+          <ServerSearch
+            data={[
+              {
+                label: 'Chat Channels',
+                type: 'channel',
+                data: textChannels.map(c => ({
+                  id: c.id,
+                  name: c.name,
+                  icon: iconMap[c.type]
+                }))
+              },
+              {
+                label: 'Audio Channels',
+                type: 'channel',
+                data: audioChannels.map(c => ({
+                  id: c.id,
+                  name: c.name,
+                  icon: iconMap[c.type]
+                }))
+              },
+              {
+                label: 'Video Channels',
+                type: 'channel',
+                data: videoChannels.map(c => ({
+                  id: c.id,
+                  name: c.name,
+                  icon: iconMap[c.type]
+                }))
+              },
+              {
+                label: 'Members',
+                type: 'member',
+                data: members.map(m => ({
+                  id: m.id,
+                  name: m.profile.name,
+                  icon: roleToIcon[m.role]
+                }))
+              }
+            ]}
+          />
         </div>
-        <Separator className='bg-zinc-200 dark:bg-zinc-700 rounded my-2' />
+        <Separator className="my-2 rounded bg-zinc-200 dark:bg-zinc-700" />
         {!!textChannels.length && (
-          <div className='mb-2'>
+          <div className="mb-2">
             <ServerSection
-              sectionType='channel'
+              sectionType="channel"
               channelType={CHANNEL_TYPE.TEXT}
               role={role}
-              label='Text channels'
+              label="Text channels"
             />
             {textChannels.map(channel => (
-              <ServerChannel key={channel.id} role={role} channel={channel} server={server} />
+              <ServerChannel
+                key={channel.id}
+                role={role}
+                channel={channel}
+                server={server}
+              />
             ))}
           </div>
         )}
         {!!audioChannels.length && (
-          <div className='mb-2'>
+          <div className="mb-2">
             <ServerSection
-              sectionType='channel'
+              sectionType="channel"
               channelType={CHANNEL_TYPE.AUDIO}
               role={role}
-              label='Audio channels'
+              label="Audio channels"
             />
             {audioChannels.map(channel => (
-              <ServerChannel key={channel.id} role={role} channel={channel} server={server} />
+              <ServerChannel
+                key={channel.id}
+                role={role}
+                channel={channel}
+                server={server}
+              />
             ))}
           </div>
         )}
